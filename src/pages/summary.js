3e01403639e9ee1c3a0ea1a692a4454b35ade3aa
codekyd <okpusjamin@gmail.com>
2020-12-01 00:19:14 +0100

    update readme

--- src/pages/summary.js
+++ src/pages/summary.js
@@ -3,7 +3,7 @@ import dayjs from 'dayjs';
 
 const Summary = ({ location, history }) => {
   // *pull the passed data from the current location
-  const { fullName, message, date }  = location.state
+  const { fullName, message, date, theme }  = location.state
   return (
     <div className="container">
       <div className="content justify-center">
@@ -14,13 +14,13 @@ const Summary = ({ location, history }) => {
                 <p>{ message }</p>
               </blockquote>
             </div>
-            <div className="card-footer calm-ui">
+            <div className={`card-footer ${theme}`}>
               <h3> - { fullName } </h3>
               <p>{ dayjs(date).format('MMM DD,YYYY') }</p>
             </div>
           </div>
           <div className="btn-group">
-          <button type="" className="btn" onClick={() => history.goBack()}>Back</button>
+          <button type="" className="btn" onClick={ history.goBack}>Back</button>
           <button type="submit" className="btn btn-primary">Save</button>
         </div>
         </div>
